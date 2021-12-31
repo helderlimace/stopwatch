47d2452fe6d0874d77501399bde68afd798bd127
Helder Lima <helder.fl@hotmail.com>
2021-12-31 14:00:54 -0300

    refactor

--- src/script.js
+++ src/script.js
@@ -1,46 +1,37 @@
-function cron(){
-
-    function converterSegundosParaHora(seg){
-        let hora = new Date(seg * 1000);
-        const options = {hour12:false, timeZone: 'UTC'};
-        hora = hora.toLocaleTimeString('pt-br', options);
-        return hora;
-    }
-
-    let relogio = document.querySelector('.clock');
-    relogio.innerHTML = '00:00:00';
-
-    let count = 1;
-    let time;
-
-    function cronometro(){
-        
-        time = setInterval(() => {
-            relogio.innerHTML = converterSegundosParaHora(count);
-            count++;
-        }, 1000);
-    }
-
-    document.addEventListener('click', function(e){
-        let el = e.target;
-
-        if(el.classList.contains('iniciar')){
-            cronometro();
-        }
-
-        if(el.classList.contains('pausar')){
-            clearInterval(time);
-        }
-
-        if(el.classList.contains('reiniciar')){
-            clearInterval(time);
-            count = 1;
-            relogio.innerHTML = '00:00:00';
-        }
-    });
-
-    converterSegundosParaHora();
-
+function convertSecondsToHours(seconds){
+    let date = new Date(seconds * 1000);
+    const options = {hour12: false, timeZone: 'UTC'};
+    date = date.toLocaleTimeString('pt-br', options)
+    return date;
 }
 
-cron();
+let clock = document.querySelector('.clock');
+let count = 1;
+let interval;
+
+function meter(){
+    interval = setInterval(function(){
+        clock.innerHTML = convertSecondsToHours(count);
+        count++;
+    }, 1000);
+}
+
+document.addEventListener('click', function(e){
+    let el = e.target;
+
+    if(el.classList.contains('start')){
+        meter();
+    }
+
+    if(el.classList.contains('pause')){
+        clearInterval(interval);
+    }
+
+    if(el.classList.contains('restart')){
+        clearInterval(interval);
+        clock.innerHTML = '00:00:00';
+    }
+});
+
+
+
